c1785eeb53db287223b68b912471d55de5e98676
NAMENAMENAME <x41n3.n30@gmail.com>
2025-07-09 04:17:28 +0200

    blueJ Code start thing

---
+++ Objekt.java
@@ -0,0 +1,17 @@
+/**
+ * Write a description of class Objekt here.
+ *
+ * @author Leo 
+ * @version 09.07.2025
+ */
+public class Objekt
+{
+    public Objekt()
+    {
+    }
+
+
+    public void sampleMethod()
+    {
+    }
+}
